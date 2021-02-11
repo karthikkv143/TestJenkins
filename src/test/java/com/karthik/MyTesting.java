e4ca68fb24e9f9faf164d5aaef68397e0b5ee551
Venkata Karthik Koripalli <Karthik.Koripalli@ivycomptech.com>
2021-02-11 23:10:32 +0530

    First commit

---
+++ src/test/java/com/karthik/MyTesting.java
@@ -0,0 +1,21 @@
+package com.karthik;
+
+import org.testng.annotations.Test;
+
+public class MyTesting {
+	@Test
+	public void test1() {
+		System.out.println("====Testcase 1=======");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("====Testcase Two======");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("====Testcase three======");
+	}
+
+}
